602c30b5ac418e85871ee7c5ab7f72c49ec5ac55
KryptonMiner <79050258+KryptonMiner@users.noreply.github.com>
2022-12-20 17:06:45 +0100

    Update console_lib.lua

--- console_lib.lua
+++ console_lib.lua
@@ -1,9 +1,11 @@
+
 do
-	local Console = game:GetService("CoreGui"):FindFirstChild("STX_Console")
-	if Console then
-		Console:Destroy()
-	end
+    local Console = game:GetService("CoreGui"):FindFirstChild("STX_Console")
+    if Console then
+        Console:Destroy()
+    end
 end
+
 
 local Console = {}
 
