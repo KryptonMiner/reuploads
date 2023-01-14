651890a2fc7486e022341f29daad7adb98716583
KryptonMiner <79050258+KryptonMiner@users.noreply.github.com>
2023-01-14 15:11:04 +0100

    Update lib_backup.lua

--- lib_backup.lua
+++ lib_backup.lua
@@ -49,9 +49,9 @@ finity.dark_theme = { -- dark
 	category_button_background = Color3.fromRGB(63, 62, 65),
 	category_button_border = Color3.fromRGB(72, 71, 74),
 
-	checkbox_checked = Color3.fromRGB(138, 112, 214),
-	checkbox_outer = Color3.fromRGB(173, 126, 194),
-	checkbox_inner = Color3.fromRGB(227, 181, 255),
+	checkbox_checked = Color3.fromRGB(112, 84, 184),
+	checkbox_outer = Color3.fromRGB(22, 14, 33),
+	checkbox_inner = Color3.fromRGB(22, 14, 33),
 
 	slider_color = Color3.fromRGB(177, 177, 177),
 	slider_color_sliding = Color3.fromRGB(132, 255, 130),
